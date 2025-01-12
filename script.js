2a2eadbe511e1455cb1129ba6d448c1f8c890692
dileepdongala <dileep.dongala@gmail.com>
2025-01-12 12:10:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ function generateQuotation() {
 <div style="flex-direction: column; background-color: rgb(46, 48, 48);">
     <div style="text-align: center;">
         <div style="display: block;">
-            <img src="logo.jpeg" alt="Company Logo" style="width:100px; margin-bottom:20px; margin-bottom:20px;">
+            <img src="logo.jpeg" alt="Company Logo" style="width:100px; margin-bottom:20px; margin-bottom:20px !important;">
         </div>
     </div>
 
@@ -179,13 +179,27 @@ function generateQuotation() {
     
         });
 
-       // <h3>Complimentary Items</h3>
+        /*  <h3>Complimentary Items</h3>
 
-       // <ul>${complimentary.map(item => `<li>${item}</li>`).join("")}</ul>
+        <ul>${complimentary.map(item => `<li>${item}</li>`).join("")}</ul>
 
-    // <h3>Deliverables</h3>
+     <h3>Deliverables</h3>
 
-               // <ul style="list-style-type:none">${deliverables.map(item => `<li>${item}</li>`).join("")}</ul>
+     <ul style="list-style-type:none">${deliverables.map(item => `<li>${item}</li>`).join("")}</ul>
+
+   <h3>Charges</h3>
+
+                <p>Total Cost: ${totalCost}</p>
+
+                <p>50% advance, remaining 50% on shoot day.</p>
+
+                <h3>Hard Drives</h3>
+
+                <p>You must provide 2x 8TB external hard drives.</p>*
+                
+                 <h3>Terms & Conditions</h3>
+
+                <ul style="list-style-type:none">${terms.map(term => `<li>${term}</li>`).join("")}</ul> */
 
        quoteHTML += ` 
         </div>
@@ -314,20 +328,218 @@ function generateQuotation() {
                     </div>
                 </div>
             </div>   
+            <div style="display: flex; width: 100%; gap: 40px; padding: 20px 0px;">
+                <div
+                    style="display: flex; flex-direction: column; text-align: center; word-break: break-word; min-width: 880px; max-width: 880px;">
+                    <div>
+                        <p style="line-height: 1.2;"><span
+                                style="font-family: freight-text-pro, serif; color: rgb(249, 203, 156); font-size: 36px;">Charges</span>
+                        </p>
+                        <p style="line-height: 1.2;"><br></p>
+                        <p style="line-height: 1.2;"><span
+                                style="font-family: Georgia, serif; color: rgb(252, 229, 205); font-size: 36px;">₹&nbsp;<span
+                                    style="border-bottom:none; border:none; background:none;">${totalCost}</span></span>
+                        </p>
+                        <p style="line-height: 1.2;"><span
+                                style="font-family: Georgia, serif; color: rgb(252, 229, 205); font-size: 18px;"><br></span>
+                        </p>
+                        <p><span style="font-family: Georgia, serif; color: rgb(252, 229, 205); font-size: 18px;">+GST
+                                18%</span></p>
+                        <p style="text-align: left;"><span
+                                style="font-family: Georgia, serif; color: rgb(252, 229, 205); font-size: 18px;"><br></span>
+                        </p>
+                        <p style="text-align: center;"><span
+                                style="font-family: Georgia, serif; color: rgb(252, 229, 205); font-size: 18px;">50%
+                                to be paid in advance to block the dates.</span></p>
+                        <p style="text-align: center;"><span
+                                style="font-family: Georgia, serif; color: rgb(252, 229, 205); font-size: 18px;">✧</span>
+                        </p>
+                        <p style="text-align: center;"><span
+                                style="font-family: Georgia, serif; color: rgb(252, 229, 205); font-size: 18px;">
+                                Remaining 50% Payment shall be done on last day of shoot.</span></p>
+                        <p style="text-align: center;"><span
+                                style="font-family: Georgia, serif; color: rgb(252, 229, 205); font-size: 18px;">✧</span>
+                        </p>
+                        <p style="text-align: center;"><span
+                                style="font-family: Georgia, serif; color: rgb(252, 229, 205); font-size: 18px;">Deliverables
+                                timeline starts from the date of full payment clearance.</span></p>
+                    </div>
+                </div>
+            </div>
 
-                <h3>Charges</h3>
+            <div style="display: flex; width: 100%; gap: 40px; padding: 20px 0px;">
+                <div
+                    style="display: none; flex-direction: column; text-align: center; word-break: break-word; min-width: 880px; max-width: 880px;">
+                    <div style="height: 100%;">
+                        <div>
+                            <p><span style="font-family: freight-text-pro, serif; font-size: 36px;">Hard
+                                    Drives</span></p>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    style="display: flex; flex-direction: column; text-align: center; word-break: break-word; min-width: 880px; max-width: 880px;">
+                    <div style="height: 100%;">
+                        <div>
+                            <p><span
+                                    style="font-family: freight-text-pro, serif; color: rgb(249, 203, 156); font-size: 36px;">Hard
+                                    Drives</span></p>
+                            <p style="text-align: left;"><span
+                                    style="font-family: Georgia, serif; color: rgb(252, 229, 205); font-size: 18px;">✧
+                                    You shall provide 2 units of&nbsp;&nbsp;8&nbsp;TB&nbsp;&nbsp;external hard
+                                    drives to store all the data and backup</span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-                <p>Total Cost: ${totalCost}</p>
+            <div style="display: flex; width: 100%; gap: 40px; padding: 20px 0px;">
+                <div
+                    style="display: none; flex-direction: column; text-align: center; word-break: break-word; min-width: 880px; max-width: 880px;">
+                    <div style="height: 100%;">
+                        <div>
+                            <p><br></p>
+                            <p><br></p>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    style="display: flex; flex-direction: column; text-align: center; word-break: break-word; min-width: 880px; max-width: 880px;">
+                    <div>
+                        <p style="text-align: center;"><span
+                                style="font-family: freight-text-pro, serif; color: rgb(249, 203, 156); font-size: 36px;">Additional
+                                Services</span></p>
+                        <p style="text-align: left;"><span style="font-family: Georgia, serif; font-size: 18px;">
+                                <br> <span style="color: rgb(252, 229, 205);">Add-on services provided by our
+                                    partner&nbsp;vendor,&nbsp;&nbsp;that can greatly enhance your event
+                                    experience.</span></span></p>
+                        <p style="text-align: left;"><span
+                                style="font-family: Georgia, serif; color: rgb(252, 229, 205); font-size: 18px;"><br></span>
+                        </p>
+                        <p style="text-align: left;"><span
+                                style="font-family: Georgia, serif; color: rgb(252, 229, 205); font-size: 18px;"><strong>LED
+                                    Screens</strong> </span></p>
+                        <p style="text-align: left;"><span
+                                style="font-family: Georgia, serif; color: rgb(252, 229, 205); font-size: 18px;"><br></span>
+                        </p>
+                        <p style="text-align: left;"><span
+                                style="font-family: Georgia, serif; color: rgb(252, 229, 205); font-size: 18px;">✧
+                                Charges : ₹&nbsp;&nbsp;20,000&nbsp;per LED Screen</span></p>
+                        <p style="text-align: left;"><span
+                                style="font-family: Georgia, serif; color: rgb(252, 229, 205); font-size: 18px;">
+                                <br> <strong>Web Live</strong> </span></p>
+                        <p style="text-align: left;"><span
+                                style="font-family: Georgia, serif; color: rgb(252, 229, 205); font-size: 18px;"><br></span>
+                        </p>
+                        <p style="text-align: left;"><span
+                                style="font-family: Georgia, serif; color: rgb(252, 229, 205); font-size: 18px;">✧
+                                Charges : ₹&nbsp;&nbsp;15,000&nbsp;per session ( 4 hours )</span></p>
+                    </div>
+                </div>
+            </div>
 
-                <p>50% advance, remaining 50% on shoot day.</p>
+            <div style="display: flex; flex-direction: column; width: 100%; gap: 40px; padding: 20px 0px;">
+                <div
+                    style="display: flex; flex-direction: column; text-align: center; word-break: break-word; min-width: 880px; max-width: 880px;">
+                    <div>
+                        <p><span
+                                style="font-family: freight-text-pro, serif; font-size: 36px; color: rgb(249, 203, 156);">Terms
+                                and Conditions<br></span></p>
+                    </div>
+                </div>
+                <div
+                    style="display: flex; flex-direction: column; text-align: center; word-break: break-word; min-width: 880px; max-width: 880px;">
+                    <div style="height: 100%;">
+                        <div>
+                            <p style="text-align: left;"><span
+                                    style="font-family: Georgia, serif; line-height: 1; color: rgb(252, 229, 205);">✧
+                                    For events held at locations distant from our office, you shall
+                                    arrange&nbsp; travel &amp; accommodation for our shoot crew to ensure
+                                    seamless service</span></p>
+                            <p style="text-align: left;"><span
+                                    style="font-family: Georgia, serif; line-height: 1; color: rgb(252, 229, 205);"><br></span>
+                            </p>
+                            <p style="text-align: left;"><span
+                                    style="font-family: Georgia, serif; color: rgb(252, 229, 205);"> ✧
+                                    Cancellation after advance payment &amp; team reservation will incur a 25%
+                                    fee of the total order value. The remaining amount will be refunded within
+                                    90 days&nbsp;</span></p>
+                            <p style="text-align: left;"><span
+                                    style="font-family: Georgia, serif; color: rgb(252, 229, 205);"><br></span>
+                            </p>
+                            <p style="text-align: left;"><span
+                                    style="font-family: Georgia, serif; color: rgb(252, 229, 205);"> ✧ Our
+                                    Pricing is completely based on the list of events you provided ,ensuring
+                                    that&nbsp;you're only charged for the services you need.</span></p>
+                            <p style="text-align: left;"><span
+                                    style="font-family: Georgia, serif; color: rgb(252, 229, 205);"><br></span>
+                            </p>
+                            <p style="text-align: left;"><span
+                                    style="font-family: Georgia, serif; color: rgb(252, 229, 205);">✧ Any
+                                    addition of new events will incur additional charges.</span></p>
+                            <p style="text-align: left;"><span
+                                    style="font-family: Georgia, serif; color: rgb(252, 229, 205);"><br></span>
+                            </p>
+                            <p style="text-align: left;"><span
+                                    style="font-family: Georgia, serif; color: rgb(252, 229, 205);">✧ Any change
+                                    of plan or postponement of any events will be accomodated with the best team
+                                    available.&nbsp;</span></p>
+                            <p style="text-align: left;"><span
+                                    style="font-family: Georgia, serif; color: rgb(252, 229, 205);"><br></span>
+                            </p>
+                            <p style="text-align: left;"><span
+                                    style="font-family: Georgia, serif; color: rgb(252, 229, 205);"> ✧ We hold
+                                    all the copyrights to use photos and videos in our social media &amp;
+                                    promotional platforms.</span></p>
+                            <p style="text-align: left;"><span
+                                    style="font-family: Georgia, serif; color: rgb(252, 229, 205);"><br></span>
+                            </p>
+                            <p style="text-align: left;"><span
+                                    style="font-family: Georgia, serif; line-height: 1.4; color: rgb(252, 229, 205);">
+                                    ✧ While we’ve never lost any event's data in the past 10 years, in the
+                                    rarest probability of any data loss, we are liable to shoot another event
+                                    for free or deduct the corresponding event charges.</span></p>
+                            <p style="text-align: left;"><span
+                                    style="font-family: Georgia, serif; line-height: 1.4; color: rgb(252, 229, 205);"><br></span>
+                            </p>
+                            <p style="text-align: left;"><span
+                                    style="font-family: Georgia, serif; color: rgb(252, 229, 205);"> ✧ Re edits
+                                    - Photos &amp; Videos - you can mention any number of changes and get it
+                                    done within 15 days from the day of delivery. </span></p>
+                            <p style="text-align: left;"><span
+                                    style="font-family: Georgia, serif; color: rgb(252, 229, 205);"><br></span>
+                            </p>
+                            <p style="text-align: left;"><span
+                                    style="font-family: Georgia, serif; color: rgb(252, 229, 205);">✧ After all
+                                    the photos and videos are delivered to you, we will consider the project as
+                                    closed &amp; cannot accommodate any further work requests.</span></p>
+                            <p style="text-align: left;"><span
+                                    style="font-family: Georgia, serif; color: rgb(252, 229, 205);"><br></span>
+                            </p>
+                            <p style="text-align: left;"><span
+                                    style="font-family: Georgia, serif; color: rgb(252, 229, 205);"> ✧ Once your
+                                    data is delivered through HDDs, we’ll not own any of your information. We
+                                    consider that you’re the sole owner of your data and the complete
+                                    responsibility of it lies with you.</span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-                <h3>Hard Drives</h3>
-
-                <p>You must provide 2x 8TB external hard drives.</p>
-
-                <h3>Terms & Conditions</h3>
-
-                <ul style="list-style-type:none">${terms.map(term => `<li>${term}</li>`).join("")}</ul>
+            
+            <div style="display: flex; flex-direction: column;padding: 20px 0px; z-index: 1; width: 100%; gap: 40px;">
+                <div
+                    style="display: flex; flex-direction: column; text-align: center; color: rgb(0, 0, 0); word-break: break-word;">
+                    <div style="height: 100%;">
+                        <div>
+                            <p style="font-family: Georgia, serif; font-weight: 400; font-size: 22px;"><span
+                                    style="color: rgb(252, 229, 205);">Congratulations,</span></p>
+                            <p style="font-family: Georgia, serif; font-weight: 400; font-size: 22px;"><span
+                                    style="color: rgb(252, 229, 205);">Knoty Weddings</span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
             
         </div>
     </div>
